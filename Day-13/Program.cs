dde1bf11691212f1b8f02ae581f53c84d5ee4dab
ANMOL <anmol999rajput@gmail.com>
2022-03-24 19:21:08 +0530

    Adding Generic

--- Day-13/Program.cs
+++ Day-13/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace Day_13
 {
@@ -6,7 +9,33 @@ namespace Day_13
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Generics Problem");
+            int num1, num2, num3;
+            // set the value of the three numbers
+            Console.WriteLine("Enter the first number : ");
+            num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number : ");
+            num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the third number : ");
+            num3 = int.Parse(Console.ReadLine());
+          
+
+            if (num1 > num2)
+            {
+                if (num1 > num3)
+                {
+                    Console.Write("Number one is the Maximum!\n");
+                }
+                else
+                {
+                    Console.Write("Number three is the Maximum!\n");
+                }
+            }
+            else if (num2 > num3)
+                Console.Write("Number two is the Maximum!\n");
+            else
+                Console.Write("Number three is the Maximum!\n");
         }
     }
 }
